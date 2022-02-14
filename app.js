7cebb045a5e08f53458f5f7064bcf21b7ec42710
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-14 13:20:54 +0530

    creating custom list using express

--- app.js
+++ app.js
@@ -35,8 +35,12 @@ const item3=new Item({
 
 const defaultItems=[item1,item2,item3];
 
+const listSchema={
+  name:String,
+  items:[itemsSchema]
+};
 
-
+const List=mongoose.model("List",listSchema);
 
 app.get("/", function(req, res) {
 
@@ -62,6 +66,31 @@ Item.find({},function(err,foundItems) {
 
 });
 
+app.get("/:customListName",function(req,res){
+  const customListName=req.params.customListName;
+
+  List.findOne({name:customListName},function(err,foundList) {
+    if(!err){
+      if(!foundList){
+        const list=new List({
+          name:customListName,
+          items:defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+
+      }else{
+        res.render("list",{listTitle:foundList.name,newListItems:foundList.items})
+      }
+    }
+  });
+
+
+
+});
+
+
 app.post("/", function(req, res){
   const itemName=req.body.newItem;
 
@@ -70,6 +99,17 @@ app.post("/", function(req, res){
   });
   item.save();
   res.redirect("/");
+});
+
+app.post("/delete",function(req,res) {
+  const checkedItemId=req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId,function(err){
+    if(!err){
+      console.log("successs");
+    }
+  });
+
 });
 
 app.get("/work", function(req,res){
